512acd83b27617ebc9b11cb6a06eb0d5e74169c4
Patryk Batog <pbatog1@driv.com>
2024-07-15 23:14:19 +0200

    chore: Refactor fetch URL in script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-fetch("https://userdatabase-d4a6.restdb.io/rest/users?key=669580b8232bada2fde4d002", {
-  method: "GET", // or POST, PUT, DELETE, etc.
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-    Origin: "https://simplethtmltest-c6bcftjra-patryks-projects-3dac6345.vercel.app",
-  },
-})
-  .then((response) => {
+async function fetchUsers() {
+  try {
+    const response = await fetch("https://userdatabase-d4a6.restdb.io/rest/users?key=669580b8232bada2fde4d002", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      mode: "cors", // Make sure mode is set to 'cors'
+    });
+
     if (!response.ok) {
       throw new Error("Network response was not ok " + response.statusText);
     }
-    return response.json();
-  })
-  .then((data) => console.log(data))
-  .catch((error) => console.error("Error fetching users:", error));
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+}
+
+fetchUsers();
