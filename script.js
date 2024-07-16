0486a5d2b37b35625a2296d9fb10fc2b0e1d6c42
Patryk Batog <pbatog1@driv.com>
2024-07-16 18:45:39 +0200

    Refactor fetch logic and update API key in script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const API_KEY = "pat2vyyvaf2AIREq1.a63aeb49c64a3d009439291ec53082e518f6ffced9eae7428a9610bea0355686";
-const BASE_ID = "appdtQMz1VcWrGaY2";
-const TABLE_NAME = "Users";
-const URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
+const API_KEY = "66959486232badc18de4d136";
+const URL = `https://userdatabase-d4a6.restdb.io/rest/users`;
 
 fetch(URL, {
   method: "GET",
   headers: {
-    Authorization: `Bearer ${API_KEY}`,
     "Content-Type": "application/json",
+    "x-apikey": API_KEY,
+    // "Cache-Control": "no-cache",
   },
 })
   .then((response) => {
@@ -18,9 +17,9 @@ fetch(URL, {
   })
   .then((data) => {
     const userList = document.getElementById("user-list");
-    data.records.forEach((record) => {
+    data.forEach((user) => {
       const listItem = document.createElement("li");
-      listItem.textContent = record.fields.Name; // Assuming the name field is "Name"
+      listItem.textContent = user.name;
       userList.appendChild(listItem);
     });
   })
